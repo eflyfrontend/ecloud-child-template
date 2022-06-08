754f06ff826c1b8ae4f551f91e8ea07047eabc4c
xiaohuiguo <1836250418@qq.com>
2022-06-08 11:32:11 +0800

    添加meta，增加测试参数

--- src/components/common/footer/footer.ts
+++
@@ -1,7 +0,0 @@
-import {Component, Prop, Vue, Emit, Watch} from 'vue-property-decorator';
-
-@Component
-export default class AdminFooter extends Vue {
-    @Prop({default: () => 'fixed'}) private type!: any;
-    private padHeight: any = '74';
-}
